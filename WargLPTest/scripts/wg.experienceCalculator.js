e4ea3f1d87afe9a7d994a0609681e1e41a777842
nikolai80 <nicholasrabota@gmail.com>
2017-01-23 23:11:02 +0300

    Создание репозитория

---
+++ WargLPTest/scripts/wg.experienceCalculator.js
@@ -0,0 +1,14 @@
+﻿var wg = wg || {}
+
+wg.expCalculator = {
+	options: {
+		equipment: "",
+		experience: "",
+		battleCount:""
+	},
+	init: function (options) {
+		wg.expCalculator.options = $.extend(wg.expCalculator.options, options)
+		
+	},
+	expirienceCalc:function(){}
+}
